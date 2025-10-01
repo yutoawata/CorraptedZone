a1818eb5cb371fdbecc3fd2dee6d41d11fc3ef79
awatayuto <yulovecat9@gmail.com>
2025-10-01 14:44:54 +0900

    自機の基本的な動きを実装
    移動処理・回転処理を実装
    それに伴うゲームパッドの入力管理クラスを作成

---
+++ SurvivalFPS/Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    Gamepad gamepad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamepad = Gamepad.current;
+    }
+
+    public Vector2 InputRightStickValue() { return gamepad.rightStick.value; }
+    public Vector2 InputLeftStickValue() { return gamepad.leftStick.value; }
+    public bool IsInputUpButton() { return gamepad.buttonNorth.isPressed; }
+    public bool IsInputDownButton() { return gamepad.buttonSouth.isPressed; }
+    public bool IsInputRightButton() { return gamepad.buttonEast.isPressed; }
+    public bool IsInputLeftButton() { return gamepad.buttonWest.isPressed; }
+}
