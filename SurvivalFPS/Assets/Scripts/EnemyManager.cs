532b6ec8b8217e65ea8bd1842e10122b6dc81af7
Sota Sakurai <qqzp47vd@gmail.com>
2025-10-01 13:18:50 +0900

    ステージ上に敵を生成する

--- SurvivalFPS/Assets/Scripts/EnemyManager.cs
+++ SurvivalFPS/Assets/Scripts/EnemyManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField] GameObject enemyObject = null;
+    [SerializeField] GameObject stageObject = null;
+    [SerializeField] float spawnOffsetY = 0.0f;
 
     float timer = 0.0f;
 
@@ -22,9 +24,16 @@ public class EnemyManager : MonoBehaviour
         // 3•bŒo‰ß‚ÅoŒ»
         if (timer > spawnTime)
         {
+            // stage‚Ì‘å‚«‚³‚ðŽæ“¾‚·‚é
+            Renderer stageRenderer = stageObject.GetComponent<Renderer>();
+            Bounds stageBounds = stageRenderer.bounds;
+            float randPosX = Random.Range(stageBounds.min.x, stageBounds.max.x);
+            float randPosZ = Random.Range(stageBounds.min.z, stageBounds.max.z);
+            Vector3 spawnPos = new Vector3(randPosX, stageBounds.max.y + spawnOffsetY, randPosZ);
+
             Instantiate(
                 enemyObject,
-                transform.position,
+                spawnPos,
                 Quaternion.identity);
 
             timer = 0.0f;
