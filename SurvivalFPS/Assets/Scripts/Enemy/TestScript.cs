d3a80cd8601a403dbda6e4763e53f88e5fafa7f8
Sota Sakurai <qqzp47vd@gmail.com>
2025-10-03 14:53:27 +0900

    敵と障害物に重ならずスポーンするように修正

--- SurvivalFPS/Assets/Scripts/Enemy/TestScript.cs
+++ SurvivalFPS/Assets/Scripts/Enemy/TestScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    GameObject manager = null;
+    EnemyManager enemyManager = null;
+
+    float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("EnemyManager");
+        enemyManager = manager.GetComponent<EnemyManager>();
+        enemyManager.RegistrationEnemyList(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //timer += Time.deltaTime;
+        //float destroyTime = 3.0f;
+        //if (timer > destroyTime)
+        //{
+        //    enemyManager.RemoveEnemyList(gameObject);
+        //    Destroy(gameObject);
+        //}
     }
 }
