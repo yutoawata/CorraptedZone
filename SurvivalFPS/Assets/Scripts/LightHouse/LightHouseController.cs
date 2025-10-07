41e2af0162d83a549457a168b58b1865bbf4418b
Sota Sakurai <qqzp47vd@gmail.com>
2025-10-07 13:05:00 +0900

    灯台が一定角度を向いたときに敵が生成される

--- SurvivalFPS/Assets/Scripts/LightHouse/LightHouseController.cs
+++ SurvivalFPS/Assets/Scripts/LightHouse/LightHouseController.cs
@@ -7,7 +7,18 @@ public class LightHouseController : MonoBehaviour
     [SerializeField]private Transform[] targetItem;     //アイテムの座標取得に使用
     [SerializeField]private int spotNumber;             //照らすアイテムの番号
     [SerializeField]private int spotIndex;              //アイテム配列の要素数の取得に使用
+    [SerializeField]private GameObject enemyObject;
 
+    private EnemyManager enemyManager;
+
+    // 追加 スポーンを管理する変数
+    bool hasSpawned;
+
+    private void Awake()
+    {
+        // 追加
+        enemyManager = enemyObject.GetComponent<EnemyManager>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +39,31 @@ public class LightHouseController : MonoBehaviour
         spotNumber = Mathf.FloorToInt(Time.time / spotChangeSpan) % spotIndex;
         // ターゲット方向のベクトルを取得
         Vector3 relativePos = ReturnEnemyTargetTransform() - this.transform.position;
+        // 追加 ターゲット方向の回転情報を格納
+        Quaternion targetRotation = Quaternion.LookRotation(relativePos);
         //回転軸の固定(横にだけ動かすため)
         relativePos.y = 0;
         //位置座標から角度に変換
         Quaternion rotation = Quaternion.LookRotation(relativePos);
         // 現在の回転情報と、ターゲット方向の回転情報を補完する
         transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, rotationSpeed);
+
+        // 追加 現在の角度と目標回転角の差を格納
+        float angleDeff = Quaternion.Angle(transform.rotation, targetRotation);
+
+        // 追加 一定角度内を向くと敵が生成されるようになる マジックナンバーは角度
+        if (angleDeff < 5.0f && !hasSpawned)
+        {
+            enemyManager.SpawnEnemy();
+            hasSpawned = true;
+            Debug.Log(hasSpawned);
+        }
+
+        // 追加 一定角度から離れると生成が出来る状態に戻す
+        if (angleDeff > 10.0f)
+        {
+            hasSpawned = false;
+        }
     }
 
     public Vector3 ReturnEnemyTargetTransform()
