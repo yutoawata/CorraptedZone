236dc342e45088d0ae8c75b25f8f159b1417f51c
Knazawa-Yuusei <yuutaku607@gmail.com>
2025-10-09 17:19:18 +0900

    ゲームオーバーシーンからのシーン切り替えの実装

---
+++ SurvivalFPS/Assets/Scripts/Scene/GameOverController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    bool isReturnTitle;
+    bool isRestartGame;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isReturnTitle = false;
+        isRestartGame = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.IsInputUpButton())
+        {
+            isReturnTitle = true;
+        }
+        if (InputManager.IsInputLeftButton())
+        {
+            isRestartGame = true;
+        }
+
+        if(isReturnTitle)
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+        if (isRestartGame)
+        {
+            SceneManager.LoadScene("Kanazawa");
+        }
+    }
+}
--- SurvivalFPS/Assets/Scripts/Scene/SceneController.cs
+++ SurvivalFPS/Assets/Scripts/Scene/SceneController.cs
@@ -8,18 +8,76 @@ using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject exitCanvas;
+    [SerializeField] private LightEmissionController lightEmissionController;
+    bool isOpenPause;
+    bool isReturnGame;
+    bool isExitGame;
 
-    public void EndGame(bool isClear)
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        if (isClear)
+        exitCanvas.SetActive(false);
+        ResetBool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current != null)
         {
-            // ロードするシーン
-            SceneManager.LoadScene("");
+            if (InputManager.IsSelectButton())
+            {
+                isOpenPause = true;
+            }
+            if (InputManager.IsInputRightButton() && isOpenPause)
+            {
+                isExitGame = true;
+            }
+            if (InputManager.IsInputDownButton() && isOpenPause)
+            {
+                isReturnGame = true;
+            }
         }
-        else
+
+
+        if (isOpenPause)
         {
-            // ロードするシーン
-            SceneManager.LoadScene("");
+            Time.timeScale = 0.0f;
+            exitCanvas.SetActive(true);
+
+            if (isReturnGame)
+            {
+                Time.timeScale = 1.0f;
+                isOpenPause = false;
+                isReturnGame = false;
+                exitCanvas.SetActive(false);
+                Debug.Log("ゲーム続行");
+            }
+
+            if (isExitGame)
+            {
+                Debug.Log("ゲーム終了");
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+                Application.Quit();//ゲームプレイ終了
+#endif
+            }
         }
+
+        if (lightEmissionController.Fuel >= 0.0f)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+
+    void ResetBool()
+    {
+        isOpenPause = false;
+        isReturnGame = false;
+        isExitGame = false;
     }
 }
