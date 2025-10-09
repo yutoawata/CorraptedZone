324944618b8c6578a09d0ba77b10c10df1eefb63
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-10-09 13:00:15 +0900

    リポップ機能の追加 + フィールドアイテムの配置

--- SurvivalFPS/Assets/Scripts/FieldItem/ExplosionDrumCan.cs
+++ SurvivalFPS/Assets/Scripts/FieldItem/ExplosionDrumCan.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ExplosionDrumCan : MonoBehaviour, IRayCastHit
 {
-    [SerializeField] ExplosionManager explosionManager;
+    [SerializeField] GameObject child;
+    ExplosionManager explosionManager;
+    bool isActive = true;
+    float timer = 0;
+
+    void Awake()
+    {
+        explosionManager = GameObject.Find("ExplosionManager").GetComponent<ExplosionManager>();
+    }
 
 
     [Header("”š”­‚Ì‘å‚«‚³")]
-    [SerializeField] float explosionMul;
+    [SerializeField] float explosionMul = 1;
 
 
     public void OnRaycastHit(RaycastHit hit)
     {
-        explosionManager.Generate(transform.position.x, transform.position.y, transform.position.z, explosionMul);
+        if (isActive)
+        {
+            explosionManager.Generate(child.transform.position.x, child.transform.position.y, child.transform.position.z, explosionMul);
+            child.gameObject.SetActive(false);
+            isActive = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 10)
+            {
+                isActive = true;
+                child.SetActive(true);
+                timer = 0;
+            }
+        }
     }
 }
