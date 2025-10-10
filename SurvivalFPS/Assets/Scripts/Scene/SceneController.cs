9ef08819316f9000d133184966a639c3057cf335
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-10-10 11:08:30 +0900

    カーソル非表示

--- SurvivalFPS/Assets/Scripts/Scene/SceneController.cs
+++ SurvivalFPS/Assets/Scripts/Scene/SceneController.cs
@@ -21,6 +21,7 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = false;
         timeController.StartTimer();
         exitCanvas.SetActive(false);
         ResetBool();
